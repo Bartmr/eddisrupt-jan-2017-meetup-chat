12a177d0e44658f12df1486572429ce735157ca5
Bartolomeu Rodrigues <pbartmr@gmail.com>
2018-01-15 23:22:20 +0000

    i think its done

--- static/index.js
+++ static/index.js
@@ -6,8 +6,14 @@ var inputField = document.getElementById('input-field')
 
 var socket = io()
 
-inputContainer.addEventListener("submit", function () {
-  socket.emit('chat message', inputField.value)
+inputContainer.addEventListener("submit", function (event) {
+  event.preventDefault()
+
+  var valueToSend = inputField.value
+
+  inputField.value = ''
+  
+  socket.emit('chat message', valueToSend)
 })
 
 socket.on('chat message', function(msg){
